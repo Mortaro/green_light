23e3d8b8308752147c741519d6e98ad713faf777
unknown <christian.mortaro@gmail.com>
2013-05-08 21:51:03 -0300

    fixing format typo in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  match "/javascripts/green-light.js" => "assets#green_light", via: [:get, :post]
+  match "/javascripts/green-light" => "assets#green_light", via: [:get, :post]
   match "/javascripts/check_for_uniqueness" => "javascripts#check_for_uniqueness", via: [:get, :post]
 end
